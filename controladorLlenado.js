a2f0ca8d5819694e116bd0ab55cf32dd9482873d
501 <501@academicos.cesde.edu.co>
2022-01-20 08:34:39 -0600

    se crea objetos

--- controladorLlenado.js
+++ controladorLlenado.js
@@ -1,13 +1,13 @@
 let productos=[
     {nombre:"Aguardiente tapa roja",tamano:"1/2 botella",precio:200000,foto:"img/img1.png"},
-    {nombre:"Aguardiente tapa azul",tamano:"1/2 botella",precio:220000,foto:""},
-    {nombre:"Tequila Don Julio",tamano:"1 botella",precio:300000,foto:""},
-    {nombre:"Cerveza Aguila",tamano:"1 botella",precio:8000,foto:""},
-    {nombre:"Cerveza Pilsen",tamano:"1 botella",precio:7000,foto:""},
-    {nombre:"Ron Medellin",tamano:"1/2 botella",precio:220000,foto:""},
-    {nombre:"Ron Añejo 220 años de Caldas",tamano:"1/2 botella",precio:100000,foto:""},
-    {nombre:"Agua",tamano:"1 botella",precio:4000,foto:""},
-    {nombre:"Soda",tamano:"1 botella",precio:6000,foto:""},
-    {nombre:"Gatorade",tamano:"1 botella",precio:8000,foto:""},
+    {nombre:"Aguardiente tapa azul",tamano:"1/2 botella",precio:220000,foto:"img/img2.png"},
+    {nombre:"Tequila Don Julio",tamano:"1 botella",precio:300000,foto:"img/img3.png"},
+    {nombre:"Cerveza Aguila",tamano:"1 botella",precio:8000,foto:"img/img4.png"},
+    {nombre:"Cerveza Pilsen",tamano:"1 botella",precio:7000,foto:"img/img5.png"},
+    {nombre:"Ron Medellin",tamano:"1/2 botella",precio:220000,foto:"img/img6.png"},
+    {nombre:"Ron Añejo 220 años de Caldas",tamano:"1/2 botella",precio:100000,foto:"img/img7.png"},
+    {nombre:"Agua",tamano:"1 botella",precio:4000,foto:"img/img8.png"},
+    {nombre:"Soda",tamano:"1 botella",precio:6000,foto:"img/img9.png"},
+    {nombre:"Gatorade",tamano:"1 botella",precio:8000,foto:"img/img10.png"},
     
 ]
